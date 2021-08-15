25e418f69f914e88a96cf79633265c5201832462
marcusong <marcusong@capps.com.sg>
2021-08-15 14:56:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# US States Game
+This project is about creating US States guessing game using Python.
+
+![alt-text ](us-states-game.gif)
---
+++ main.py
@@ -0,0 +1,50 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+
+def write_state(state_txt):
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == state_txt]
+    t.goto(x=int(state_data.x), y=int(state_data.y))
+    # t.write(state_data.state.item())
+    t.write(state_txt)
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="Key in the state's name").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                write_state(state)
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        write_state(answer_state)
+        # t = turtle.Turtle()
+        # t.hideturtle()
+        # t.penup()
+        # state_data = data[data.state == answer_state]
+        # t.goto(x=int(state_data.x), y=int(state_data.y))
+        # # t.write(state_data.state.item())
+        # t.write(answer_state)
+
+
+turtle.mainloop()
